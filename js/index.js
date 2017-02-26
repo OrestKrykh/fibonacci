fcf94317f22237da518d02b7bfe8a0d781986c32
orest krykh <orest.krykh@gmail.com>
2017-02-26 18:58:09 +0000

    added recurs

--- js/index.js
+++ js/index.js
@@ -1,10 +1,25 @@
 var number =Number(prompt('введіть n-порядок числа Фібоначі',number));
-function fibonacci(number){
-    var number,fib;
-    fib =[0,1];
-    for (var i =2; i <=number; i++){
-        fib[i] = fib[i-1] + fib[i-2];
-    }return fib[number]
-}
-document.write(fibonacci(number));
+// function fibonacci(number){
+//     var number,fib;
+//     fib =[0,1];
+//     for (var i =2; i <=number; i++){
+//         fib[i] = fib[i-1] + fib[i-2];
+//     }return fib[number]
+// }
+// document.write(fibonacci(number));
 
+function fibonacciRecyrs(number){
+    var fib;
+    fib=[];
+    if (number ===1){
+        return 1
+    }
+    if (number ===2){
+        return 1
+    }else{
+        return fibonacciRecyrs(number - 1) + fibonacciRecyrs(number - 2);
+    }
+}
+    
+    
+document.write(fibonacciRecyrs(number));
