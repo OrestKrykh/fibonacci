e00fc68a988b19d12a800ef179fa4ff3488a1009
orest krykh <orest.krykh@gmail.com>
2017-02-27 16:15:14 +0000

    changed

--- js/index.js
+++ js/index.js
@@ -1,12 +1,14 @@
 var number =Number(prompt('введіть n-порядок числа Фібоначі',number));
-// function fibonacci(number){
-//     var number,fib;
-//     fib =[0,1];
-//     for (var i =2; i <=number; i++){
-//         fib[i] = fib[i-1] + fib[i-2];
-//     }return fib[number]
-// }
-// document.write(fibonacci(number));
+function fibonacci(number){
+    var number,fib;
+    fib =[0,1];
+    for (var i =2; i <=number; i++){
+        fib[i] = fib[i-1] + fib[i-2];
+    }return fib[number]
+}
+document.write(fibonacci(number));
+document.write('<br>');
+
 
 function fibonacciRecyrs(number){
     var fib;
